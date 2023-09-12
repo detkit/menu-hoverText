5cba3a040fe23e272ba68ad9419f751581f9798f
Dang Quang <quangthat97@gmail.com>
2023-09-13 05:44:42 +0700

    first menu Hover

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const text = document.querySelectorAll('ul li a').forEach((text) => {
+	text.innerHTML = text.innerText
+		.split('')
+		.map((letter, i) => `<span>${letter}</span>`)
+		.join('');
+
+	let span = document.querySelectorAll('ul li a span').forEach((e) => {
+		let duration = Math.random() * 5;
+		e.style.animationDuration = 0.25 + duration + 's';
+		e.style.animationDelay = 0.25 + duration + 's';
+	});
+});
